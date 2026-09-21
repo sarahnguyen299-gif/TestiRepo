03d0bec7fa2bb00c2e5ca522f42aa3da3a216120
sarahnguyen299-gif <sarahnguyen299@gmail.com>
2026-09-21 13:21:04 +0300

    Update shop.py
    uusi omminaisuus lisatty

--- shop.py
+++ shop.py
@@ -1,2 +1,18 @@
 print ("Let's buy something")
 
+def go_shopping():
+    print()
+    print("What do you want to buy?")
+    print("1. Sword")
+    print("2. Apple")
+ 
+    choice = int(input("Choose what to buy (1/2): "))
+ 
+    if choice == 1:
+        print("you bought a sword!")
+        return "Sword"
+    elif choice == 2:
+        print("you bought an apple!")
+        return "Apple"
+    else:
+        print("You haven't chosen")
