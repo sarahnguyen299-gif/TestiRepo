5be26b6dab804780a9aaff8d1315689e7dc19d1f
sarahnguyen299-gif <sarahnguyen299@gmail.com>
2026-09-21 13:01:43 +0300

    Create forest.py
    lisatty forsest omminaisu

---
+++ forest.py
@@ -0,0 +1,10 @@
+def enter_forest():
+    print("You enter the forest")
+    print("A very big and angry troll appears front of you")
+
+    choice = input("Do you want to fight? (yes/no)):")
+
+    if (choice == "yes"):
+        print("You fight the troll and your ass is kicked!")
+    else:
+        print("You escape back to village.")
